f47a62139fb74531fcdecd565ad254807e67adab
Nik <nik_cro@abv.bg>
2018-09-18 07:57:30 +0300

    Updating TimeUtil to support multiple time formats.

--- src/main/java/com/github/lordrex34/config/util/TimeUtil.java
+++ src/main/java/com/github/lordrex34/config/util/TimeUtil.java
@@ -23,24 +23,16 @@ package com.github.lordrex34.config.util;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * @author UnAfraid
+ * @author UnAfraid, Nik
  */
 public class TimeUtil
 {
-	public static int findIndexOfNonDigit(CharSequence text)
-	{
-		for (int i = 0; i < text.length(); i++)
-		{
-			if (Character.isDigit(text.charAt(i)))
-			{
-				continue;
-			}
-			return i;
-		}
-		return -1;
-	}
+	/** Pattern that matches texts similar to "1hour30min20sec" into multiple groups of digit and non-digit parts. */
+	public static final Pattern PARSE_DURATION_PATTERN = Pattern.compile("(\\d+)([^\\d]+)");
 	
 	/**
 	 * Parses patterns like:
@@ -51,80 +43,131 @@ public class TimeUtil
 	 * <li>1month or 12months</li>
 	 * <li>1year or 5years</li>
 	 * </ul>
+	 * Also multiple value types in pattern are supported as well:
+	 * <ul>
+	 * <li>1hour30min20sec</li>
+	 * <li>3days22hours20mins</li>
+	 * <li>1week5days6hours</li>
+	 * <li>10hours20mins30secs500millis200micros100nanos</li>
+	 * </ul>
+	 * Supported time units:
+	 * <ul>
+	 * <li>nanos</li>
+	 * <li>micros</li>
+	 * <li>millis</li>
+	 * <li>sec</li>
+	 * <li>secs</li>
+	 * <li>min</li>
+	 * <li>mins</li>
+	 * <li>hour</li>
+	 * <li>hours</li>
+	 * <li>day</li>
+	 * <li>days</li>
+	 * <li>week</li>
+	 * <li>weeks</li>
+	 * <li>month</li>
+	 * <li>months</li>
+	 * <li>year</li>
+	 * <li>years</li>
+	 * <li>All enum names of {@link ChronoUnit} by auto-converting input time unit to upper case before matching.</li>
+	 * </ul>
 	 * @param datePattern
 	 * @return {@link Duration} object converted by the date pattern specified.
 	 * @throws IllegalStateException when malformed pattern specified.
 	 */
 	public static Duration parseDuration(String datePattern)
 	{
-		final int index = findIndexOfNonDigit(datePattern);
-		if (index == -1)
-		{
-			throw new IllegalStateException("Incorrect time format given: " + datePattern);
-		}
 		try
 		{
-			int val = Integer.parseInt(datePattern.substring(0, index));
-			final String type = datePattern.substring(index);
-			final ChronoUnit unit;
-			switch (type.toLowerCase())
+			Duration result = null;
+			final Matcher matcher = PARSE_DURATION_PATTERN.matcher(datePattern);
+			while (matcher.find())
 			{
-				case "sec":
-				case "secs":
+				final long value = Long.parseLong(matcher.group(1));
+				final String type = matcher.group(2);
+				final ChronoUnit unit;
+				switch (type.toLowerCase())
 				{
-					unit = ChronoUnit.SECONDS;
-					break;
-				}
-				case "min":
-				case "mins":
-				{
-					unit = ChronoUnit.MINUTES;
-					break;
-				}
-				case "hour":
-				case "hours":
-				{
-					unit = ChronoUnit.HOURS;
-					break;
-				}
-				case "day":
-				case "days":
-				{
-					unit = ChronoUnit.DAYS;
-					break;
-				}
-				case "week":
-				case "weeks":
-				{
-					unit = ChronoUnit.WEEKS;
-					break;
-				}
-				case "month":
-				case "months":
-				{
-					unit = ChronoUnit.MONTHS;
-					break;
-				}
-				case "year":
-				case "years":
-				{
-					unit = ChronoUnit.YEARS;
-					break;
-				}
-				default:
-				{
-					unit = ChronoUnit.valueOf(type);
-					if (unit == null)
+					case "nanos":
 					{
-						throw new IllegalStateException("Incorrect format: " + type + " !!");
+						unit = ChronoUnit.NANOS;
+						break;
+					}
+					case "micros":
+					{
+						unit = ChronoUnit.MICROS;
+						break;
+					}
+					case "millis":
+					{
+						unit = ChronoUnit.MILLIS;
+						break;
+					}
+					case "sec":
+					case "secs":
+					{
+						unit = ChronoUnit.SECONDS;
+						break;
+					}
+					case "min":
+					case "mins":
+					{
+						unit = ChronoUnit.MINUTES;
+						break;
+					}
+					case "hour":
+					case "hours":
+					{
+						unit = ChronoUnit.HOURS;
+						break;
+					}
+					case "day":
+					case "days":
+					{
+						unit = ChronoUnit.DAYS;
+						break;
+					}
+					case "week":
+					case "weeks":
+					{
+						unit = ChronoUnit.WEEKS;
+						break;
+					}
+					case "month":
+					case "months":
+					{
+						unit = ChronoUnit.MONTHS;
+						break;
+					}
+					case "year":
+					case "years":
+					{
+						unit = ChronoUnit.YEARS;
+						break;
+					}
+					default:
+					{
+						unit = ChronoUnit.valueOf(type.toUpperCase());
+						if (unit == null)
+						{
+							throw new IllegalArgumentException("Incorrect unit of time: " + type + "!");
+						}
 					}
 				}
+
+				result = result == null ? Duration.of(value, unit) : result.plus(value, unit);
 			}
-			return Duration.of(val, unit);
+
+			if (result == null)
+			{
+				throw new IllegalStateException("Time format has failed to produce results!");
+			}
+
+			return result;
 		}
 		catch (Exception e)
 		{
-			throw new IllegalStateException("Incorrect time format given: " + datePattern + " val: " + datePattern.substring(0, index));
+			throw new IllegalStateException("Incorrect time format given: " + datePattern + "!", e);
 		}
 	}
 }
